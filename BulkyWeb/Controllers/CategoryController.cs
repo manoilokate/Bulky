5dbbbe01eb6b010b78163f6d8efc5cf55e1d3a96
manoilokate <kateryna.manoilo@lnu.edu.ua>
2024-06-16 15:37:51 +0300

    custom validations

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -26,6 +26,10 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The display order cannot exactly match the name");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
