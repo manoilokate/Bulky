7ae28a7c3a7c7b9dcdeac9752a1a747db81fb5fd
manoilokate <kateryna.manoilo@lnu.edu.ua>
2024-06-28 11:59:42 +0300

    UnitOfWork implementation

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,5 +1,6 @@
 using Bulky.DataAccess;
 using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,7 +11,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=> 
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<ICategoryRepository, ICategoryRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
